e607a1bcf490c8fd4825fd53e2a52e51b1ce88bb
JugurtaO <jugurtaourzik@gmail.com>
2023-02-22 22:10:04 +0100

    +master: bug fixed at config.js

--- config/config.js
+++ config/config.js
@@ -3,21 +3,14 @@ const mysql = require('mysql');
 
 
 module.exports.db_handler = mysql.createConnection({
-//   host: process.env.DB_host,
-// user: process.env.DB_user,
-// password: process.env.DB_password,
-// database: process.env.DB_database
-  host: "localhost",
-  user:"root",
-  password:"password",
-  database:"TasksList"
+  host: process.env.DB_host,
+user: process.env.DB_user,
+password: process.env.DB_password,
+database: process.env.DB_database
+
 
 
 })
 
 
 
-// host: process.env.DB_host,
-// user: process.env.DB_user,
-// password: process.env.DB_password,
-// database: process.env.DB_database
