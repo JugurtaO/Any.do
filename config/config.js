9db2a4e95c05fc06ab220fdfd2d16b4e2c999f76
JugurtaO <jugurtaourzik@gmail.com>
2023-03-26 23:01:26 +0200

    +master: fixed authentication failure on railway MySQL

--- config/config.js
+++ config/config.js
@@ -2,15 +2,6 @@
 const mysql = require('mysql');
 
 
-// module.exports.db_handler = mysql.createConnection({
-// host: String(process.env.DB_host),
-// user: String(process.env.DB_user),
-// password: String(process.env.DB_password),
-// database: String(process.env.DB_database),
-// port:process.env.DB_port
-
-
-// })
 
 module.exports.db_handler= mysql.createConnection(String(process.env.DB_url));
 
