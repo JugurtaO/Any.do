b594aed95f5d519861102da315758fec77749292
JugurtaO <jugurtaourzik@gmail.com>
2023-05-19 23:38:37 +0200

    +master: gathering controllers in index.js file

---
+++ Controllers/index.js
@@ -0,0 +1,5 @@
+module.exports.addTask=require("./addTask").addTask;
+module.exports.deleteTask=require("./addTask").deleteTask;
+module.exports.editTask=require("./addTask").editTask;
+module.exports.viewEditPage=require("./addTask").viewEditPage;
+module.exports.viewTasks=require("./addTask").viewTasks;
