b6d17d7e280e5c43ca220840a8d2c59ab03d9f64
JugurtaO <jugurtaourzik@gmail.com>
2023-06-01 08:42:51 +0200

    +master: changes in config.js for deployment

--- config/config.js
+++ config/config.js
@@ -2,17 +2,17 @@
 const mysql = require('mysql');
 
 
-//in case of production use the commented line below
-// module.exports.db_handler= mysql.createConnection(String(process.env.DB_url));  
+// in case of production use the commented line below
+module.exports.db_handler= mysql.createConnection(String(process.env.DB_url));  
 
 //otherwise use the object connection as shown below:
 
 
-module.exports.db_handler= mysql.createConnection({
-    host:String(process.env.DB_host),
-    user:String(process.env.DB_user),
-    database:String(process.env.DB_database) ,
-    password:String(process.env.DB_password) 
+// module.exports.db_handler= mysql.createConnection({
+//     host:String(process.env.DB_host),
+//     user:String(process.env.DB_user),
+//     database:String(process.env.DB_database) ,
+//     password:String(process.env.DB_password) 
     
-});  
+// });  
 
