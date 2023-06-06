98901bb8598fd179013871015361ffdb673ff0df
JugurtaO <jugurtaourzik@gmail.com>
2023-06-06 16:02:10 +0200

    +master: fixed sql error in signup-signout controllers

--- config/config.js
+++ config/config.js
@@ -1,6 +1,4 @@
-
-const mysql = require('mysql');
-
+const mysql=require("mysql");
 
 // in case of production use the commented line below
 module.exports.db_handler= mysql.createConnection(String(process.env.DB_url));  
