f7f876b5d86052bc3c07c070969fb75c6828ba52
JugurtaO <jugurtaourzik@gmail.com>
2023-06-08 14:48:26 +0200

    +master: fixed error in login controller

--- config/config.js
+++ config/config.js
@@ -3,7 +3,6 @@ const mysql=require("mysql");
 // in case of production use the commented line below
 module.exports.db_handler= mysql.createConnection(String(process.env.DB_url));  
 
-//otherwise use the object connection as shown below:
 
 
 // module.exports.db_handler= mysql.createConnection({
